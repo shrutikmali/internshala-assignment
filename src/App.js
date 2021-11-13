115597c1b82ca5482dacf7946970fb6794744091
Shrutik <shrutik.mali@somaiya.edu>
2021-11-13 15:03:17 +0530

    Add background image

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import NavBar from './NavBar/NavBar';
 import Section from './Section/Section';
 import Footer from './Footer/Footer';
 import './index.css';
+import Student from './student2.png';
 
 const App = () => {
   return (
     <>
-      <div className='background'></div>
+      <div className='background'>
+        <img src={Student} width={300} className='bgImage'/>
+      </div>
       <NavBar />
       <Section />
       <Footer />
